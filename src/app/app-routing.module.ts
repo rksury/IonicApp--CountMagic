c959ee9c11285575d3d0141aafe321082f96867f
rksury <rksury@gmail.com>
2020-02-19 17:35:25 +0530

    completed processing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,32 +1,34 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
-   { path: '', redirectTo: '/login', pathMatch: 'full' },
+    {path: '', redirectTo: '/login', pathMatch: 'full'},
 
-  {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
-  },
-  {
-    path: 'signup',
-    loadChildren: () => import('./signup/signup.module').then(m => m.SignupPageModule)
-  },
-  {
-    path: 'processing',
-    loadChildren: () => import('./processing/processing.module').then( m => m.ProcessingPageModule)
-  },
+    {
+        path: '',
+        loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    },
+    {
+        path: 'login',
+        loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
+    },
+    {
+        path: 'signup',
+        loadChildren: () => import('./signup/signup.module').then(m => m.SignupPageModule)
+    },
+    {
+        path: 'processing',
+        loadChildren: () => import('./processing/processing.module').then(m => m.ProcessingPageModule)
+    },
 
 
 ];
+
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})
+    ],
+    exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
