b79dc7f9f27f5aac2482ccf7e9bb63f6113cad31
ayush3298 <abairagi311@gmail.com>
2020-02-29 06:56:38 +0530

    update login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -10,12 +10,16 @@ import {LoginService} from '../All.Services/login/login.service';
 })
 export class LoginPage implements OnInit {
 
-    LoginForm: FormGroup;
+    // submitform: FormGroup;
     loading = false;
     submitted = false;
-    email: string;
-    password: string;
+    // email: string;
+    // password: string;
 
+    submitform = new FormGroup({
+        email: new FormControl(''),
+        password: new FormControl(''),
+    });
 
     constructor(private router: Router, private formBuilder: FormBuilder,
                 private loginService: LoginService, private route: ActivatedRoute) {
@@ -26,34 +30,34 @@ export class LoginPage implements OnInit {
     }
 
     ngOnInit() {
-        this.LoginForm = this.formBuilder.group({
+        this.submitform = this.formBuilder.group({
             email: new FormControl('', Validators.compose([
                 Validators.required, Validators.email, Validators.pattern
                 ('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
             ])),
-           password: ['', [Validators.required, Validators.minLength(6)]]
+            password: ['', [Validators.required, Validators.minLength(6)]]
         });
     }
 
-    get function() {
-        return this.LoginForm.controls;
+    // get function() {
+    //     return this.submitform.controls;
+    // }
+
+    // LoginSubmit() {
+    //     this.submitted = true;
+    //
+    //     // stop here if form is invalid
+    //     if (this.LoginForm.invalid) {
+    //         // console.log('123456789')
+    //         return this.LoginForm;
+    //     } else {
+    //         this.loginService.Login(this.email, this.password);
+    //     }
+    // }
+
+    onSubmit() {
+        console.warn(this.submitform.value);
     }
-
-    LoginSubmit() {
-        this.submitted = true;
-
-        // stop here if form is invalid
-        if (this.LoginForm.invalid) {
-            // console.log('123456789')
-            return this.LoginForm;
-        } else {
-            this.loginService.Login(this.email, this.password);
-        }
-
-
-    }
-
-
 }
 
 
